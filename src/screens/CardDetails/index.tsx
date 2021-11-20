83418fca26862dc1fbbbed0904a06d9a22d0bcef
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2021-11-20 19:44:44 -0300

    Continuando a interface de Detalhes

--- src/screens/CardDetails/index.tsx
+++ src/screens/CardDetails/index.tsx
@@ -1,27 +1,58 @@
 import React from 'react';
- import { BackButton } from '../../components/BackButton';
+import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../ImageSlider';
 
 
 import {
   Container,
   Header,
-  CarImages
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
+
 } from './styles';
 
 export function CardDetails() {
   return (
     <Container>
       <Header>
-        <BackButton onPress={() =>{} }  />
+        <BackButton onPress={() => { }} />
       </Header>
 
-
       <CarImages>
-       <ImageSlider imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}   />
-       </CarImages>
+        <ImageSlider imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário
+          touro de lide indultado na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+
+        </About>
+
+      </Content>
 
     </Container>
   );
 }
- 
